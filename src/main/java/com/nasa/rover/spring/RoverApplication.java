582876f26f0fec0ee34ebbb1226bf0dbbfe976ad
Nadeeja <nadeejas@zone24x7.com>
2020-09-24 22:32:59 +0530

    initial commit 🔥

---
+++ src/main/java/com/nasa/rover/spring/RoverApplication.java
@@ -0,0 +1,13 @@
+package com.nasa.rover.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoverApplication.class, args);
+	}
+
+}
